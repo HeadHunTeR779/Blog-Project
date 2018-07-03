825f03a1dd464fde5ff9cbdaa61169b3ace7c3e6
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-07-03 02:30:58 -0200

    Added Posting Functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    url(r'^$',views.PostListView.as_view(),name="home"), #HOme page
+    url(r'^$',views.PostListView.as_view(),name="post_list"), #HOme page
     url(r'^about/$', views.AboutView.as_view(), name="about"),  #This is the index page
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name="post_detail"), #DOMAIN/detail/pk/
+    url(r'^post/new/$', views.PostCreateView.as_view(), name="post_new"), #DOMAIN/post/new/
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 from django.utils import timezone
-from django.views.generic import (TemplateView, ListView)
+from django.contrib.auth.decorators import login_required #this decorator ONLY for functions !!
+from django.contrib.auth.mixins import LoginRequiredMixin #use this Mix-in in classes
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 # Create your views here.
 class AboutView(TemplateView):
@@ -19,3 +22,18 @@ class PostListView(ListView):
         return Post.objects.filter(published_date__lte = timezone.now()).orderby("-published_date")
         # default would have been Post.objects.all() notice in orderby I passed a negative sign for DESCENDING
         #else OLDER posts would have come first
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post_detail"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    login_url = "/login/"  #In case the person is not logged where should they go?
+    redirect_field_name = "blog/post_detail.html"
+    form_class = PostForm  #Remember this guy passes its own object named form which has trivial stuff if you wanna send your own form then OVERRIDE
+    context_object_name = "form"  #now it sends PostForm in object name of form
+
+    model = Post
+    template_name = "blog/create_post.html"
