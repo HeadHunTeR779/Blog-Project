e3559238969854b9f3ccb90a788b8120aad9e591
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-07-10 15:10:18 -0200

    Added CSS keystrokes animation

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views   #FOr built-in login and logout
 
+app
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')),   #Anything that is not Domain/admin goto blog.urls
